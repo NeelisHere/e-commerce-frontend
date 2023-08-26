bde7ce44fb8dd162ec2d5fb983818b52a7582619
Neelabh Paul <npaul_be19@thapar.edu>
2023-08-26 11:47:14 +0530

    cart-page issues fixed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import SingupPage from "./pages/SingupPage";
 import ErrorPage from "./pages/ErrorPage";
+import CartPage from "./pages/CartPage";
 
 const router = createBrowserRouter([
 	{ path: '/', element: <Home />, errorElement: <ErrorPage/>},
 	{ path: '/login', element: <LoginPage />},
-	{ path: '/sign-up', element: <SingupPage />}
+	{ path: '/sign-up', element: <SingupPage />},
+	{ path: '/cart', element: <CartPage />},
 ])
 
 function App() {
